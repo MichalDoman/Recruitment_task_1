4af08005d7515e07925384e829625cd16801ca11
michaldoman <mjd.domanski@gmail.com>
2024-01-26 17:58:21 +0100

    models/settings set up

--- recruitment_task_1/task/models.py
+++ recruitment_task_1/task/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+from djongo import models
 
-# Create your models here.
+
+class Parts(models.Model):
+    serial_number = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+    price = models.FloatField()
+    location = models.JSONField(max_length=255)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    parent_name = models.CharField(max_length=255, null=True
+                                   )
